fc06ef36fb49647650568275ca5f21195d0eed14
halfwa <yaroslav.shamov2@mail.ru>
2023-07-01 14:51:27 +0300

    Добавляет проект решения задачи 13.6.1

---
+++ CSharp/Module13/Module13/Task_13_6_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+namespace Task_13_6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "C:\\Users\\HP\\Desktop\\Downloads\\text.txt";
+            var symbols = File.ReadAllText(path).ToCharArray();   
+
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+            List<char> chars = new List<char>(); // 24ms
+            foreach (char s in symbols)
+            {
+                chars.Add(s);
+      
+            }
+
+            //LinkedList<char> chars = new LinkedList<char>(); // 1400ms
+
+            //var firstChar = chars.AddFirst(' ');
+
+            //foreach (char s in symbols)
+            //{
+            //    chars.AddAfter(firstChar,s);   
+            //}
+
+            sw.Stop();
+
+            Console.WriteLine($"Time: {sw.ElapsedMilliseconds}");
+            Console.ReadKey();
+        }
+    }
+}
