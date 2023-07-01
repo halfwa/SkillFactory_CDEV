d78ff5337f7ab7372cb3734ff7c11dffd1297592
halfwa <yaroslav.shamov2@mail.ru>
2023-07-01 16:47:49 +0300

    Добавляет решение проекта задания 16.6.2

---
+++ CSharp/Module13/Task_13_6_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+
+namespace Task_13_6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "C:\\Users\\HP\\Desktop\\Downloads\\text.txt";
+            var text = File.ReadAllText(path);
+
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+            string[] words = noPunctuationText.Split(new char[] { ' ' });
+
+            var wordDictionary = new Dictionary<string,int>(); 
+
+            foreach (var word in words) 
+            {
+                if (!wordDictionary.ContainsKey(word))
+                {
+                    wordDictionary.Add(word, 1);
+                }
+                else
+                {
+                    wordDictionary[word]++;
+                }
+            }
+
+            var result = wordDictionary.Values.OrderDescending().Take(10);
+
+            foreach (var count in result)
+            {
+                Console.WriteLine(count);
+            }
+            Console.ReadKey();
+        }
+    }
+}
