95d21909ba00a17b8b2536e1d7ac7e709add3360
halfwa <yaroslav.shamov2@mail.ru>
2023-07-14 18:32:22 +0300

    Добавляет решение проекта задания 15.6.6

---
+++ CSharp/Module15/Module15/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task_15_6_6
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            return (from c in classes
+                    from student in c.Students
+                    select student).ToArray();
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
